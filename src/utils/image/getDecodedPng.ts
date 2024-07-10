a785db842786c0b98f865099d1f9cd624b6dbfe7
Fiddlekins <fiddlekins@gmail.com>
2024-07-10 23:55:17 +0800

    - fixed failed decoding of some PNG images

--- src/utils/image/getDecodedPng.ts
+++ src/utils/image/getDecodedPng.ts
@@ -21,13 +21,25 @@ export function getDecodedPng(data: TypedArray): DecodedPngResult {
     order: ChannelOrder.rgba,
   });
   if (image.palette) {
-    for (let pixelIndex = 0; pixelIndex < rawBytes.length; pixelIndex++) {
-      const paletteIndex = rawBytes[pixelIndex];
-      const offset = pixelIndex * 4;
-      imageData.data[offset] = image.palette.get(paletteIndex, 0);
-      imageData.data[offset + 1] = image.palette.get(paletteIndex, 1);
-      imageData.data[offset + 2] = image.palette.get(paletteIndex, 2);
-      imageData.data[offset + 3] = image.palette.get(paletteIndex, 3);
+    // The PNG library is unreliable in terms of whether it fully decodes palette based PNGs or not
+    // As such, we check the raw byte length to determine if it has already applied the palette
+    if (rawBytes.length === image.width * image.height * 4) {
+      imageData.data.set(rawBytes);
+    } else if (rawBytes.length === image.width * image.height) {
+      for (let pixelIndex = 0; pixelIndex < rawBytes.length; pixelIndex++) {
+        const paletteIndex = rawBytes[pixelIndex];
+        const offset = pixelIndex * 4;
+        imageData.data[offset] = image.palette.get(paletteIndex, 0);
+        imageData.data[offset + 1] = image.palette.get(paletteIndex, 1);
+        imageData.data[offset + 2] = image.palette.get(paletteIndex, 2);
+        if (image.numChannels === 4) {
+          imageData.data[offset + 3] = image.palette.get(paletteIndex, 3);
+        } else {
+          imageData.data[offset + 3] = 255;
+        }
+      }
+    } else {
+      throw new Error(`Unexpected rawBytes length during PNG decoding: ${rawBytes.length}bytes for ${image.width}x${image.height} image`);
     }
   } else {
     imageData.data.set(rawBytes);
