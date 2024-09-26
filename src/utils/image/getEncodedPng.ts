88e26c7d762f38f98f59c0c9be5dbbfd3862b087
Fiddlekins <fiddlekins@gmail.com>
2024-09-26 11:30:36 +0800

    - experimental reduced bit depth on export

--- src/utils/image/getEncodedPng.ts
+++ src/utils/image/getEncodedPng.ts
@@ -3,6 +3,40 @@ import {getColourKeyFromPixel} from "./conversion/getColourKeyFromPixel.ts";
 import {getPixelFromColourKey} from "./conversion/getPixelFromColourKey.ts";
 import {scan} from "./manipulation/scan.ts";
 
+// In theory lower bit depth would reduce filesize but in practise this seems to be the opposite
+// function getReducedBitFormat(indexedData8bit: Uint8Array, colourCount: number) {
+//   let indexedData = indexedData8bit;
+//   let format = Format.uint8;
+//   const reducedBitFormats = [
+//     {bits: 1, format: Format.uint1},
+//     {bits: 2, format: Format.uint2},
+//     {bits: 4, format: Format.uint4},
+//   ]
+//   for (const reducedBitFormat of reducedBitFormats) {
+//     const {bits} = reducedBitFormat;
+//     if (2 ** bits >= colourCount) {
+//       format = reducedBitFormat.format;
+//
+//       const subValueCount = 8 / bits;
+//       const subValueSize = 2 ** bits;
+//       indexedData = new Uint8Array(indexedData8bit.length / subValueCount);
+//       for (let byteIndex = 0; byteIndex < indexedData.length; byteIndex++) {
+//         let byte = 0;
+//         const targetOffset = byteIndex * subValueCount;
+//         for (let subValueIndex = 0; subValueIndex < subValueCount; subValueIndex++) {
+//           byte *= subValueSize;
+//           byte += indexedData8bit[targetOffset + subValueIndex];
+//         }
+//         indexedData[byteIndex] = byte;
+//       }
+//     }
+//   }
+//   return {
+//     bytes: indexedData,
+//     format
+//   };
+// }
+
 export interface EncodedPngOptions {
   indexed?: boolean;
 }
