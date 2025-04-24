51e7dd2c180c2f764a736316b62ac916e2c3b629
Fiddlekins <fiddlekins@gmail.com>
2025-04-24 13:20:16 +0800

    Updated pokemon sprite previews to work with new asset format

--- src/components/PokemonImage.tsx
+++ src/components/PokemonImage.tsx
@@ -3,6 +3,12 @@ import {QuestionMarkSharp} from "@mui/icons-material";
 // const assetBaseUrl = 'https://fiddlekins.github.io/aio-pif-sprite-analyser-assets';
 const assetBaseUrl = 'https://cdn.jsdelivr.net/gh/Fiddlekins/aio-pif-sprite-analyser-assets@master';
 
+// Manually update to be in sync with the actual autogen spritesheets rather than the latest DEX data
+const spriteSheetTotalSpriteCount = 501;
+const spriteSheetSpritesPerRow = 10;
+const spriteSheetSpritesPerColumn = Math.ceil(spriteSheetTotalSpriteCount / spriteSheetSpritesPerRow);
+const spriteSheetSpriteDimensions = 96;
+
 function getUnfusedSrc(pokemonId: number) {
   return `${assetBaseUrl}/assets/basePokemon/${pokemonId}.png`;
 }
@@ -25,14 +31,40 @@ export function PokemonImage(
   }: PokemonImageProps
 ) {
   if (isFusion && headPokemonId && bodyPokemonId) {
+    // Kinda scuffed but this beats regenerating all autogen spritesheets with missing pokemon placeholders
+    if (headPokemonId > spriteSheetTotalSpriteCount) {
+      return (
+        <img
+          width={'100%'}
+          height={'100%'}
+          style={imageStyle}
+          src={getUnfusedSrc(headPokemonId)}
+        />
+      );
+    }
+    if (bodyPokemonId > spriteSheetTotalSpriteCount) {
+      return (
+        <img
+          width={'100%'}
+          height={'100%'}
+          style={imageStyle}
+          src={getUnfusedSrc(bodyPokemonId)}
+        />
+      );
+    }
+
+    const backgroundOffsetLeft = (bodyPokemonId % spriteSheetSpritesPerRow) * spriteSheetSpriteDimensions;
+    const backgroundOffsetTop = Math.floor(bodyPokemonId / spriteSheetSpritesPerRow) * spriteSheetSpriteDimensions;
+    const backgroundWidth = spriteSheetSpritesPerRow * spriteSheetSpriteDimensions;
+    const backgroundHeight = spriteSheetSpritesPerColumn * spriteSheetSpriteDimensions;
     return (
       <div
         style={{
           height: '96px',
           width: '96px',
           backgroundImage: `url(${assetBaseUrl}/assets/spritesheets_autogen/${headPokemonId}.png)`,
-          backgroundPosition: `-${(bodyPokemonId % 10) * 96}px -${Math.floor(bodyPokemonId / 10) * 96}px`,
-          backgroundSize: `${10 * 96}px ${51 * 96}px`,
+          backgroundPosition: `-${backgroundOffsetLeft}px -${backgroundOffsetTop}px`,
+          backgroundSize: `${backgroundWidth}px ${backgroundHeight}px`,
         }}
       />
     );
